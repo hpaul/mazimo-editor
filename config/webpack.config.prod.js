9f2b0a5044f08a265b3c986a843a53dbcf7e3473
Paul Hrimiuc <igomodo@gmail.com>
2017-10-08 03:48:45 +0300

    Include react into bundle

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -146,10 +146,6 @@ module.exports = {
       sourceMap: shouldUseSourceMap,
     }),
   ],
-  externals: {
-   'react': 'react',
-   'react-dom': 'reactDOM'
-  },
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
